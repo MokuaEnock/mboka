ebb9fa905b8ea33cece7b52e2c1b17c8d5613877
Enock <enock.mokua@student.moringaschool.com>
2023-05-02 17:15:27 +0300

    connect footer and header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./App.css";
+import Header from "./Components/header/header";
+import Footer from "./Components/footer/footer";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
