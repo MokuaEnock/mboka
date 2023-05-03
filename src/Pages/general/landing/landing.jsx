13727276104305037dd723b903a1d68b1e883e1c
Enock <enock.mokua@student.moringaschool.com>
2023-05-03 22:35:21 +0300

    create structure for home section 1

--- src/Pages/general/landing/landing.jsx
+++ src/Pages/general/landing/landing.jsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom";
 import "./landing.css";
 
 export default function Landing() {
-  return <main> This is the landing page</main>;
+  return (
+    <main className="landing">
+      <section id="landing-1">
+        <h1>We power organizations through the right talent</h1>
+        <p>
+          Find the right candidate for your next role by exploring our talent
+          catelogue.
+        </p>
+        <span>
+          <Link to="/signup">Post an open role</Link>
+          <Link to="/signup">Find a role</Link>
+        </span>
+      </section>
+    </main>
+  );
 }
