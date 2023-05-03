e623cbc34740ebe55640b9edd342c0771b57ebc4
Enock <enock.mokua@student.moringaschool.com>
2023-05-03 22:40:53 +0300

    create forgot password compoent

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // App.js
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./App.css";
 import Header from "./Components/header/header";
@@ -8,6 +8,7 @@ import Footer from "./Components/footer/footer";
 import Landing from "./Pages/general/landing/landing";
 import Notfound from "./Pages/general/notfound/notfound";
 import Recruiter from "./Pages/recruiter/home/home";
+import ForgotPass from "./Pages/auth/forgot";
 
 function App() {
   return (
@@ -17,6 +18,10 @@ function App() {
         <Route element={<Landing />} path="/"></Route>
         <Route element={<Notfound />} path="*"></Route>
         <Route element={<Recruiter />} path="/recruiter"></Route>
+        <Route path="/auth">
+          <Route path="login"></Route>
+          <Route path="forgot" element={<ForgotPass />}></Route>
+        </Route>
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ src/Pages/auth/forgot.jsx
@@ -0,0 +1,5 @@
+import "./auth.css";
+
+export default function ForgotPass() {
+  return <main id="forgot">Forgot password</main>;
+}
