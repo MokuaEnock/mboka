b816fa7d6aba17dd494cc9620370fd4bb2d5c000
Enock <enock.mokua@student.moringaschool.com>
2023-05-05 06:27:46 +0300

    create react signup form

--- src/Pages/auth/signup.jsx
+++ src/Pages/auth/signup.jsx
@@ -1,11 +1,77 @@
 import "./auth.css";
 
+import React, { useState } from "react";
+
 export default function FinderSignup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Form submitted:", {
+      email,
+      password,
+      firstName,
+      lastName,
+      passwordConfirmation,
+    });
+    // add your API call to submit the form data here
+  };
   return (
     <main id="finder" className="auth">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>This is the image</div>
-        <div>This is the form</div>
+        <div>
+          <label>
+            Email:
+            <input
+              type="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              required
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              required
+            />
+          </label>
+          <label>
+            First Name:
+            <input
+              type="text"
+              value={firstName}
+              onChange={(event) => setFirstName(event.target.value)}
+              required
+            />
+          </label>
+          <label>
+            Last Name:
+            <input
+              type="text"
+              value={lastName}
+              onChange={(event) => setLastName(event.target.value)}
+              required
+            />
+          </label>
+          <label>
+            Confirm Password:
+            <input
+              type="password"
+              value={passwordConfirmation}
+              onChange={(event) => setPasswordConfirmation(event.target.value)}
+              required
+            />
+          </label>
+          <button type="submit">Sign Up</button>
+        </div>
       </form>
     </main>
   );
