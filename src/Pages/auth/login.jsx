b4e41f7ade641e668f1c8ebb4abae52231faccc7
Enock <enock.mokua@student.moringaschool.com>
2023-05-05 06:40:51 +0300

    create the login compoennt

--- src/Pages/auth/login.jsx
+++ src/Pages/auth/login.jsx
@@ -1,11 +1,40 @@
 import "./auth.css";
 
+import React, { useState } from "react";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <main id="login" className="auth">
-       <form>
+      <form onSubmit={handleSubmit}>
         <div>This is the image</div>
-        <div>This is the form</div>
+        <div>
+          <label>
+            Email:
+            <input
+              type="email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              required
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              required
+            />
+          </label>
+          <button type="submit">Log In</button>
+        </div>
       </form>
     </main>
   );
