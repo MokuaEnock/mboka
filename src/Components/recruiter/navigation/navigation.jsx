3c682b7d0e1a670ffb7218f488c59505c9a9a051
Enock <enock.mokua@student.moringaschool.com>
2023-05-13 02:56:40 +0300

    style the recruiter header

--- src/Components/recruiter/navigation/navigation.jsx
+++ src/Components/recruiter/navigation/navigation.jsx
@@ -1,19 +1,46 @@
 import { Link } from "react-router-dom";
+import {
+  FaHome,
+  FaEnvelope,
+  FaBell,
+  FaSuitcase,
+  FaUserFriends,
+  FaClipboardList,
+  FaCalendarAlt,
+  FaChartBar,
+  FaCog,
+} from "react-icons/fa";
 import "./navigation.css";
 
 export default function RecruiterAside() {
   return (
     <aside className="recruiter-aside">
       <h3>Mboka</h3>
-      <Link to="/recruiter">Dashboard</Link>
-      <Link to="/recruiter/messages">Messages</Link>
-      <Link to="/recruiter/notifications">Notifications</Link>
-      <p>Recruitment</p>
-      <Link to="/recruiter/jobs">Jobs</Link>
-      <Link to="/recruiter/candidates">Candidates</Link>
-      <Link to="/recruiter/interviews">Interviews</Link>
-      <Link to="/recruiter/reports">Reports</Link>
-      <Link to="/recruiter/settings">Settings</Link>
+      <Link to="/recruiter">
+        <FaHome /> Dashboard
+      </Link>
+      <Link to="/recruiter/messages">
+        <FaEnvelope /> Messages
+      </Link>
+      <Link to="/recruiter/notifications">
+        <FaBell /> Notifications
+      </Link>
+      <p>RECRUITMENT</p>
+      <Link to="/recruiter/jobs">
+        <FaSuitcase /> Jobs
+      </Link>
+      <Link to="/recruiter/candidates">
+        <FaUserFriends /> Candidates
+      </Link>
+      <Link to="/recruiter/interviews">
+        <FaCalendarAlt /> Interviews
+      </Link>
+      <Link to="/recruiter/reports">
+        <FaChartBar /> Reports
+      </Link>
+      <Link to="/recruiter/settings">
+        <FaCog /> Settings
+      </Link>
     </aside>
   );
 }
