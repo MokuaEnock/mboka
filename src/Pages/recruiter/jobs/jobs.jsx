0457d21b15c6bfd32f9173f286066ca106d48296
Enock <enock.mokua@student.moringaschool.com>
2023-05-15 00:06:52 +0300

    create structire of jobs component

--- src/Pages/recruiter/jobs/jobs.jsx
+++ src/Pages/recruiter/jobs/jobs.jsx
@@ -1,7 +1,10 @@
 import "./jobs.css";
 
+import { FaBriefcase } from "react-icons/fa";
+
 import RecruiterHeader from "../../../Components/recruiter/header/header";
 import RecruiterAside from "../../../Components/recruiter/navigation/navigation";
+import { Link } from "react-router-dom";
 
 export default function RecruiterJobs() {
   return (
@@ -10,8 +13,25 @@ export default function RecruiterJobs() {
       <section className="recruiter-container">
         <RecruiterHeader />
         <section id="recruiter-jobs" className="recruiter-cont">
-          <div></div>
-          <aside></aside>
+          <div>
+            <Link to="" className="jobs-card"></Link>
+            <Link to="" className="jobs-card"></Link>
+            <Link to="" className="jobs-card"></Link>
+            <Link to="" className="jobs-card"></Link>
+            <Link to="" className="jobs-card"></Link>
+            <Link to="" className="jobs-card"></Link>
+            <Link to="" className="jobs-card"></Link>
+            <Link to="" className="jobs-card"></Link>
+         
+          </div>
+          <aside id="recruiter-jobs-aside">
+            <div></div>
+            <h3>Where to stranger?</h3>
+            <Link to="/recruiter/job/new">
+              <FaBriefcase />
+              Post a job
+            </Link>
+          </aside>
         </section>
       </section>
     </main>
