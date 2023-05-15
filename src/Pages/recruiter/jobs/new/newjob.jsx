f3bd8211f0066fc254a58627dfb3669514daa82d
Enock <enock.mokua@student.moringaschool.com>
2023-05-15 14:21:02 +0300

    crete structure of forms

--- src/Pages/recruiter/jobs/new/newjob.jsx
+++ src/Pages/recruiter/jobs/new/newjob.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import RecruiterHeader from "../../../../Components/recruiter/header/header";
 import RecruiterAside from "../../../../Components/recruiter/navigation/navigation";
 import "./newjob.css";
@@ -9,12 +9,14 @@ export default function RecruiterJobNew() {
   const detailsRef = useRef(null);
   const reviewRef = useRef(null);
   const promoteRef = useRef(null);
+  const [activeSection, setActiveSection] = useState(1);
 
-  const handleButtonClick = (ref) => {
+  const handleButtonClick = (ref, sectionNumber) => {
     ref.current.scrollIntoView({
       behavior: "smooth",
       block: "start",
     });
+    setActiveSection(sectionNumber);
   };
 
   return (
@@ -24,36 +26,71 @@ export default function RecruiterJobNew() {
         <RecruiterHeader />
         <section id="recruiter-new-jobs" className="recruiter-cont">
           <form>
-            <div id="job-post-title" ref={titleRef}>
+            <div
+              id="job-post-title"
+              className={activeSection === 1 ? "active" : ""}
+              ref={titleRef}
+            >
               The title and summary
             </div>
-            <div id="job-post-requirements" ref={requirementsRef}>
+            <div
+              id="job-post-requirements"
+              className={activeSection === 2 ? "active" : ""}
+              ref={requirementsRef}
+            >
               Requirements and qualifications
             </div>
-            <div id="job-post-details" ref={detailsRef}>
+            <div
+              id="job-post-details"
+              className={activeSection === 3 ? "active" : ""}
+              ref={detailsRef}
+            >
               pos details
             </div>
-            <div id="job-post-review" ref={reviewRef}>
+            <div
+              id="job-post-review"
+              className={activeSection === 4 ? "active" : ""}
+              ref={reviewRef}
+            >
               review job
             </div>
-            <div id="job-post-promote" ref={promoteRef}>
+            <div
+              id="job-post-promote"
+              className={activeSection === 5 ? "active" : ""}
+              ref={promoteRef}
+            >
               promote job
             </div>
           </form>
           <aside>
-            <button onClick={() => handleButtonClick(titleRef)}>
+            <button
+              onClick={() => handleButtonClick(titleRef, 1)}
+              className={activeSection === 1 ? "active" : ""}
+            >
               1. Title
             </button>
-            <button onClick={() => handleButtonClick(requirementsRef)}>
+            <button
+              onClick={() => handleButtonClick(requirementsRef, 2)}
+              className={activeSection === 2 ? "active" : ""}
+            >
               2. Requirements
             </button>
-            <button onClick={() => handleButtonClick(detailsRef)}>
+            <button
+              onClick={() => handleButtonClick(detailsRef, 3)}
+              className={activeSection === 3 ? "active" : ""}
+            >
               3. Details
             </button>
-            <button onClick={() => handleButtonClick(reviewRef)}>
+            <button
+              onClick={() => handleButtonClick(reviewRef, 4)}
+              className={activeSection === 4 ? "active" : ""}
+            >
               4. Review
             </button>
-            <button onClick={() => handleButtonClick(promoteRef)}>
+            <button
+              onClick={() => handleButtonClick(promoteRef, 5)}
+              className={activeSection === 5 ? "active" : ""}
+            >
               5. Promote
             </button>
           </aside>
