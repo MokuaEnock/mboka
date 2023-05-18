b35b564a6011b53cec358e02380c43847e511bb8
Enock <enock.mokua@student.moringaschool.com>
2023-05-18 17:21:09 +0300

    create the structure of the candidates

--- src/Pages/recruiter/candidates/candidates.jsx
+++ src/Pages/recruiter/candidates/candidates.jsx
@@ -1,5 +1,15 @@
+import RecruiterHeader from "../../../Components/recruiter/header/header";
+import RecruiterAside from "../../../Components/recruiter/navigation/navigation";
 import "./candidates.css";
 
 export default function RecruiterCandidates() {
-  return <main>This is the candidate</main>;
+  return (
+    <main className="recruiter">
+      <RecruiterAside />
+      <section className="recruiter-container">
+        <RecruiterHeader />
+        <section className="recruiter-cont">This are teh candidates</section>
+      </section>
+    </main>
+  );
 }
--- src/Pages/recruiter/candidates/detail/detail.jsx
+++ src/Pages/recruiter/candidates/detail/detail.jsx
@@ -1,5 +1,17 @@
+import RecruiterHeader from "../../../../Components/recruiter/header/header";
+import RecruiterAside from "../../../../Components/recruiter/navigation/navigation";
 import "./detail.css";
 
 export default function RecruiterCandidatesDetail() {
-  return <main>This is the detialed candidate page</main>;
+  return (
+    <main className="recruiter">
+      <RecruiterAside />
+      <section className="recruiter-container">
+        <RecruiterHeader />
+        <section className="recruiter-cont">
+          This is the candidate detail
+        </section>
+      </section>
+    </main>
+  );
 }
