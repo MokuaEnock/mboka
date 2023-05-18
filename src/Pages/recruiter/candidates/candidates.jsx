d6e71eeab122e5c79819206d297f3f8cda51682e
Enock <enock.mokua@student.moringaschool.com>
2023-05-18 17:08:04 +0300

    create candidates pages for recruiter

---
+++ src/Pages/recruiter/candidates/candidates.jsx
@@ -0,0 +1,5 @@
+import "./candidates.css";
+
+export default function RecruiterCandidates() {
+  return <main>This is the candidate</main>;
+}
---
+++ src/Pages/recruiter/candidates/detail/detail.jsx
@@ -0,0 +1,5 @@
+import "./detail.css";
+
+export default function RecruiterCandidatesDetail() {
+  return <main>This is the detialed candidate page</main>;
+}
