101a889502c639914376cf8546ccca624ee1b7ca
Enock <enock.mokua@student.moringaschool.com>
2023-05-20 03:00:01 +0300

    create the skeleton structure of the header

--- src/Components/seeker/header/header.jsx
+++ src/Components/seeker/header/header.jsx
@@ -1,7 +1,50 @@
+import { Link } from "react-router-dom";
+import {
+  FaBookmark,
+  FaEnvelope,
+  FaListAlt,
+  FaSearch,
+  FaThumbsUp,
+} from "react-icons/fa";
 import "./header.css";
 
 export default function SeekerHeader() {
-  return <nav className="seeker-header">
-    Seeker haeder
-  </nav>;
+  return (
+    <nav className="seeker-header">
+      <div className="seeker-h-cont">
+        <Link className="seeker-logo" to="/seeker">
+          Insunity
+        </Link>
+
+        <span>
+          <Link to="/recommendations">
+            <FaThumbsUp />
+            Reccomendations
+          </Link>
+          <Link to="/find-job">
+            <FaSearch />
+            Explore
+          </Link>
+          <Link to="/bookmarks">
+            <FaBookmark />
+            Bookmarks
+          </Link>
+          <Link to="/messages">
+            <FaEnvelope />
+            Messages
+          </Link>
+          <Link to="/applied-jobs">
+            <FaListAlt />
+            Applied Jobs
+          </Link>
+        </span>
+
+        <select>
+          <option></option>
+          <option value="seeker-profile">Profile</option>
+          <option value="seeker-settings">Settings</option>
+        </select>
+      </div>
+    </nav>
+  );
 }
