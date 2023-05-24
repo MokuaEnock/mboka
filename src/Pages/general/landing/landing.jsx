56787a36f161a183a43790219d96c7b3d92c2a5f
Enock <enock.mokua@student.moringaschool.com>
2023-05-24 23:05:33 +0300

    style the header

--- src/Pages/general/landing/landing.jsx
+++ src/Pages/general/landing/landing.jsx
@@ -2,25 +2,61 @@ import { Link } from "react-router-dom";
 import "./landing.css";
 
 export default function Landing() {
-  return (
-    <main className="landing">
-      <section id="landing-1">
-        <div className="landing-main">
-          <h1>We power organizations through the right talent</h1>
-          <p>
-            Find the right candidate for your next role by exploring our talent
-            catalogue.
-          </p>
+  function Header() {
+    return (
+      <header>
+        <div className="header">
+          <Link to="/" className="logo">
+            Insunity
+          </Link>
+
           <span>
-            <Link to="/auth/poster">Post an open role</Link>
-            <Link to="/auth/finder">Find a role</Link>
+            <Link to="/">Home</Link>
+            <Link to="/">Interviews</Link>
           </span>
+
+          <Link to="/auth/login" className="header-login">
+            Login
+          </Link>
         </div>
+      </header>
+    );
+  }
 
-        <div className="landing-main2"></div>
-      </section>
+  function Container() {
+    return (
+      <main className="landing">
+        <section id="landing-1">
+          <div className="landing-main">
+            <h1>We power organizations through the right talent</h1>
+            <p>
+              Find the right candidate for your next role by exploring our
+              talent catalogue.
+            </p>
+            <span>
+              <Link to="/auth/poster">Post an open role</Link>
+              <Link to="/auth/finder">Find a role</Link>
+            </span>
+          </div>
 
-      <section id="landing-2"></section>
-    </main>
+          <div className="landing-main2">
+            <h2>The process</h2>
+
+            <div>
+              <span></span>
+            </div>
+          </div>
+        </section>
+
+        <section id="landing-2"></section>
+      </main>
+    );
+  }
+
+  return (
+    <>
+      <Header />
+      <Container />
+    </>
   );
 }
