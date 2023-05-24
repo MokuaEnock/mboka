da08a3c3666b390e6640c28912fd1f2ed0da4274
Enock <enock.mokua@student.moringaschool.com>
2023-05-24 22:34:36 +0300

    create the structure for the lanidng page

--- src/Pages/general/landing/landing.jsx
+++ src/Pages/general/landing/landing.jsx
@@ -5,18 +5,22 @@ export default function Landing() {
   return (
     <main className="landing">
       <section id="landing-1">
-        {/* <h1>We power organizations through the right talent</h1>
-        <p>
-          Find the right candidate for your next role by exploring our talent
-          catelogue.
-        </p>
-        <span>
-          <Link to="/auth/poster">Post an open role</Link>
-          <Link to="/auth/finder">Find a role</Link>
-        </span> */}
+        <div className="landing-main">
+          <h1>We power organizations through the right talent</h1>
+          <p>
+            Find the right candidate for your next role by exploring our talent
+            catalogue.
+          </p>
+          <span>
+            <Link to="/auth/poster">Post an open role</Link>
+            <Link to="/auth/finder">Find a role</Link>
+          </span>
+        </div>
 
-        <div></div>
+        <div className="landing-main2"></div>
       </section>
+
+      <section id="landing-2"></section>
     </main>
   );
 }
