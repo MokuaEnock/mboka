eb61600825b1236ab9dec931eedfb7331f89c5e8
Enock <enock.mokua@student.moringaschool.com>
2023-05-25 01:39:15 +0300

    create the first container of interviews

--- src/Pages/seeker/interviews/interviews.jsx
+++ src/Pages/seeker/interviews/interviews.jsx
@@ -6,7 +6,28 @@ export default function SeekerInterviews() {
     return (
       <main className="seeker">
         <section className="seeker-interview">
-          This is the seeker interview
+          <div id="seeker-cont1">
+            <span id="seeker-cont1-head">
+              <h3>Your Interviews</h3>
+              <button>Filter By</button>
+            </span>
+            <div id="seeker-cont1-body">
+              <div className="seeker-interview-card">Interview 1</div>
+              <div className="seeker-interview-card">Interview 2</div>
+              <div className="seeker-interview-card">Interview 3</div>
+              <div className="seeker-interview-card">Interview 4</div>
+            </div>
+            <span id="seeker-cont1-foot">
+              <button>Back</button>
+              <span>
+                <p>1</p>
+                <p>2</p>
+                <p>3</p>
+              </span>
+              <button>Next</button>
+            </span>
+          </div>
+          <article id="seeker-cont2"></article>
         </section>
       </main>
     );
