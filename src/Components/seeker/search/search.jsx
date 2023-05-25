34c25c5b3dc170cf7e4bc07f09092d58631976cd
Enock <enock.mokua@student.moringaschool.com>
2023-05-25 18:32:44 +0300

    create the structure of teh searchcompoennt

--- src/Components/seeker/search/search.jsx
+++ src/Components/seeker/search/search.jsx
@@ -3,7 +3,19 @@ import "./search.css";
 export default function SeekerSearch() {
   return (
     <section className="seeker-search">
-      <div id="seeker-search-cont"></div>
+      <div id="seeker-search-cont">
+
+        <span className="seeker-search-conts">
+          <button>All</button>
+          <button>Data Science</button>
+          <button>Machine Learning</button>
+        </span>
+
+        <form id="seeker-search-form">
+          <input type="text" placeholder="Search for Jobs ..." />
+          <button type="submit"></button>
+        </form>
+      </div>
     </section>
   );
 }
