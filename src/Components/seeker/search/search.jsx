7c1499b87b0fff7e813b619cd95304714985b271
Enock <enock.mokua@student.moringaschool.com>
2023-05-25 18:38:03 +0300

    restructure header component

--- src/Components/seeker/search/search.jsx
+++ src/Components/seeker/search/search.jsx
@@ -4,17 +4,20 @@ export default function SeekerSearch() {
   return (
     <section className="seeker-search">
       <div id="seeker-search-cont">
+        <form id="seeker-search-form">
+          <input type="text" placeholder="Search for Jobs ..." />
+          <button type="submit"></button>
+        </form>
+
+        {/* <select>
+          <option selected>Sort By</option>
+        </select> */}
 
         <span className="seeker-search-conts">
           <button>All</button>
           <button>Data Science</button>
           <button>Machine Learning</button>
         </span>
-
-        <form id="seeker-search-form">
-          <input type="text" placeholder="Search for Jobs ..." />
-          <button type="submit"></button>
-        </form>
       </div>
     </section>
   );
