501f88f1b3bb52da75d73a56a7626196e9eedf74
Enock <enock.mokua@student.moringaschool.com>
2023-05-25 03:11:11 +0300

    install react monaco editor

--- src/Pages/seeker/interviews/detail/detail.jsx
+++ src/Pages/seeker/interviews/detail/detail.jsx
@@ -1,5 +1,11 @@
 import "./detail.css";
 
 export default function SeekerInterviewDetail() {
-  return <main className="interview-detail">Interview detail section</main>;
+  return (
+    <main className="interview-detail">
+      <section id="seeker-detail-1"></section>
+      <section id="seeker-detail-2"></section>
+      <section id="seeker-detail-3"></section>
+    </main>
+  );
 }
