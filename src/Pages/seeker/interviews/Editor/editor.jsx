698553f93029ed80f73570a413a43cadbe451ca9
Enock <enock.mokua@student.moringaschool.com>
2023-05-29 01:46:16 +0300

    desctructure the code editor

--- src/Pages/seeker/interviews/Editor/editor.jsx
+++ src/Pages/seeker/interviews/Editor/editor.jsx
@@ -30,56 +30,42 @@ const CodeEditor = () => {
     setTheme(event.target.value);
   };
 
-  const Code = () => (
-    <MonacoEditor
-      width="100%"
-      height="50%"
-      language="javascript"
-      theme={theme}
-      value={code}
-      onChange={(value) => setCode(value)}
-      options={{
-        minimap: { enabled: false },
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-        wordWrap: "on",
-      }}
-    />
-  );
-
-  const Head = () => (
-    <div id="code-editor-head">
-      <p>Time: 30mins</p>
-      <span>
-        <select name="Theme" value={theme} onChange={handleThemeChange}>
-          <option value="vs-light">VS Code Light</option>
-          <option value="vs-dark">VS Code Dark</option>
-          <option value="hc-black">High Contrast</option>
-          {/* Add more theme options here */}
-        </select>
-        <select name="language">
-          <option value="javascript">JavaScript</option>
-          <option value="python">Python</option>
-          <option value="java">Java</option>
-          {/* Add more language options here */}
-        </select>
-      </span>
-    </div>
-  );
-
-  const Output = () => (
-    <div id="code-editor-output">
-      <button onClick={runCode}>Run</button>
-      {message && <p className="error-message">{message}</p>}
-      {output && <p className="output">{output}</p>}
-    </div>
-  );
-
   return (
     <>
-      <Head />
-      <Code />
-      <Output />
+      <div id="code-editor-head">
+        <p>Time: 30mins</p>
+        <span>
+          <select name="Theme" value={theme} onChange={handleThemeChange}>
+            <option value="vs-light">VS Code Light</option>
+            <option value="vs-dark">VS Code Dark</option>
+            <option value="hc-black">High Contrast</option>
+          </select>
+          <select name="language">
+            <option value="javascript">JavaScript</option>
+            <option value="python">Python</option>
+            <option value="java">Java</option>
+          </select>
+        </span>
+      </div>
+      <MonacoEditor
+        width="100%"
+        height="50%"
+        language="javascript"
+        theme={theme}
+        value={code}
+        onChange={(value) => setCode(value)}
+        options={{
+          minimap: { enabled: false },
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+          wordWrap: "on",
+        }}
+      />
+      <div id="code-editor-output">
+        <button onClick={runCode}>Run</button>
+        {message && <p className="error-message">{message}</p>}
+        {output && <p className="output">{output}</p>}
+      </div>
     </>
   );
 };
