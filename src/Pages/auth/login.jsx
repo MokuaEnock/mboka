0ee2edaa84d2585e4c06a637db4c9a8d44cda3a7
Enock <enock.mokua@student.moringaschool.com>
2023-05-29 01:10:38 +0300

    restructur the Login container

--- src/Pages/auth/login.jsx
+++ src/Pages/auth/login.jsx
@@ -3,7 +3,7 @@ import "./auth.css";
 
 import { useState } from "react";
 
-export default function Login() {
+function Container() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
@@ -13,61 +13,61 @@ export default function Login() {
     event.preventDefault();
   };
 
-  function Container() {
-    return (
-      <main id="login" className="auth">
-        <form onSubmit={handleSubmit}>
-          <div>This is the image</div>
-          <div>
-            <h3>Hello there, Job Seeker, welcome back</h3>
-            <p>Continue from where you left off.</p>
-            <label className="auth-form-input">
-              <p>Email Address</p>
-              <input
-                type="email"
-                value={email}
-                placeholder="Email Address"
-                onChange={(event) => setEmail(event.target.value)}
-              />
-            </label>
+  return (
+    <main id="login" className="auth">
+      <form onSubmit={handleSubmit}>
+        <div>This is the image</div>
+        <div>
+          <h3>Hello there, Job Seeker, welcome back</h3>
+          <p>Continue from where you left off.</p>
+          <label className="auth-form-input">
+            <p>Email Address</p>
+            <input
+              type="email"
+              value={email}
+              placeholder="Email Address"
+              onChange={(event) => setEmail(event.target.value)}
+            />
+          </label>
 
-            <label className="auth-form-input">
-              <p>Username</p>
-              <input
-                type="text"
-                value={username}
-                placeholder="Username"
-                onChange={(event) => setUsername(event.target.value)}
-              />
-            </label>
+          <label className="auth-form-input">
+            <p>Username</p>
+            <input
+              type="text"
+              value={username}
+              placeholder="Username"
+              onChange={(event) => setUsername(event.target.value)}
+            />
+          </label>
 
-            <label className="auth-form-input">
-              <p>Password</p>
-              <input
-                type="password"
-                value={password}
-                placeholder="Password"
-                onChange={(event) => setPassword(event.target.value)}
-              />
-            </label>
+          <label className="auth-form-input">
+            <p>Password</p>
+            <input
+              type="password"
+              value={password}
+              placeholder="Password"
+              onChange={(event) => setPassword(event.target.value)}
+            />
+          </label>
 
-            <button type="submit" className="auth-form-button">
-              Continue to Insunity
-            </button>
+          <button type="submit" className="auth-form-button">
+            Continue to Insunity
+          </button>
 
-            {/* <div id="auth-form-errors">
-              {formErrors.map((error, index) => (
-                <p className="auth-form-error" key={index}>
-                  {error}
-                </p>
-              ))}
-            </div> */}
-          </div>
-        </form>
-      </main>
-    );
-  }
+          {/* <div id="auth-form-errors">
+            {formErrors.map((error, index) => (
+              <p className="auth-form-error" key={index}>
+                {error}
+              </p>
+            ))}
+          </div> */}
+        </div>
+      </form>
+    </main>
+  );
+}
 
+export default function Login() {
   return (
     <>
       <Header />
