8d8c0ac3758b2b672763471986c1e8911c76b82e
Enock <enock.mokua@student.moringaschool.com>
2023-06-03 00:17:15 +0300

    adjust them colors

--- src/Pages/general/landing/landing.jsx
+++ src/Pages/general/landing/landing.jsx
@@ -6,30 +6,9 @@ export default function Landing() {
   function Container() {
     return (
       <main className="landing" id="landing">
-        <section id="landing-1">
-          <div id="landing-1-1">
-            <h2>Find The Job That You Deserve</h2>
-            <form></form>
-
-            <span>
-              <form>
-                <input
-                  type="text"
-                  placeholder="Search job title, keywords or Company"
-                />
-                <input type="text" placeholder="Province, city or region" />
-                <button type="submit">Search</button>
-              </form>
-              <p>Browse job offers by keyword or Location</p>
-            </span>
-
-            <div>
-              <Link to="/auth/finder">For Seekers</Link>
-              <Link to="/auth/poster">For Posters</Link>
-            </div>
-          </div>
-          <div id="landing-1-2"></div>
-        </section>
+        <div id="landing-1">
+          
+        </div>
 
         <section id="landing-2">
           <div id="landing-2-1">
