6c86eacbdf1efa8c94ede8e7b020bdbddddf6ad4
Enock <enock.mokua@student.moringaschool.com>
2023-06-04 09:15:15 +0300

    add scroll effect to the header

--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -1,11 +1,30 @@
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./header.css";
 
 export default function Header() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header>
+    <header className={scrolled ? "scrolled" : ""}>
       <div className="header">
-        <Link to="/" className="logo">
+        <Link to="/" className={`logo ${scrolled ? "scrolled" : ""}`}>
           Insunity
         </Link>
 
