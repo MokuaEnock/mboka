4eaf9f5db31b46c2ed2f32ad4b6848d2c75de448
Enock <enock.mokua@student.moringaschool.com>
2023-06-07 14:53:42 +0300

    create function to post data to backend

--- src/Components/seeker/signup/work/work.jsx
+++ src/Components/seeker/signup/work/work.jsx
@@ -1,9 +1,58 @@
+import React, { useState } from "react";
 import "./work.css";
 import SeekerSectionHeader from "../../ss-header/header";
-
 import { FaPlus } from "react-icons/fa";
 
 export default function SignupSection5({ sectionRefs, handleNextSection }) {
+  const [company, setCompany] = useState("");
+  const [jobTitle, setJobTitle] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [industry, setIndustry] = useState("");
+  const [jobDescription, setJobDescription] = useState("");
+  const [responsibilities, setResponsibilities] = useState("");
+  const [achievements, setAchievements] = useState("");
+  const seeker_id = localStorage.getItem("seekerId");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = {
+      seeker_id,
+      company,
+      job_title: jobTitle,
+      start_date: startDate,
+      end_date: endDate,
+      industry,
+      description: jobDescription,
+      responsibilities,
+      achievements,
+    };
+
+    fetch("http://localhost:3000/experiences", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setCompany("");
+        setJobTitle("");
+        setStartDate("");
+        setEndDate("");
+        setIndustry("");
+        setJobDescription("");
+        setResponsibilities("");
+        setAchievements("");
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
   return (
     <form className="seeker-signup-sections" ref={sectionRefs}>
       <div className="seeker-ss-cont">
@@ -14,27 +63,47 @@ export default function SignupSection5({ sectionRefs, handleNextSection }) {
         <div className="seeker-ss3-cont">
           <label className="seeker-ss3-cont-items">
             <p>Company or Organization:</p>
-            <input type="text" />
+            <input
+              type="text"
+              value={company}
+              onChange={(e) => setCompany(e.target.value)}
+            />
           </label>
 
           <label className="seeker-ss3-cont-items">
             <p>Job Title:</p>
-            <input type="text" />
+            <input
+              type="text"
+              value={jobTitle}
+              onChange={(e) => setJobTitle(e.target.value)}
+            />
           </label>
 
           <label className="seeker-ss3-cont-items">
             <p>Start date:</p>
-            <input type="date" />
+            <input
+              type="date"
+              value={startDate}
+              onChange={(e) => setStartDate(e.target.value)}
+            />
           </label>
 
           <label className="seeker-ss3-cont-items">
             <p>End date:</p>
-            <input type="date" />
+            <input
+              type="date"
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+            />
           </label>
 
           <label className="seeker-ss3-cont-items">
             <p>Industry/Sector:</p>
-            <input type="text" />
+            <input
+              type="text"
+              value={industry}
+              onChange={(e) => setIndustry(e.target.value)}
+            />
           </label>
         </div>
 
@@ -46,17 +115,29 @@ export default function SignupSection5({ sectionRefs, handleNextSection }) {
         <div className="seeker-ss3-cont2">
           <label className="seeker-ss3-textarea">
             <p>A description of your job:</p>
-            <textarea name="thesisDescription" />
+            <textarea
+              name="jobDescription"
+              value={jobDescription}
+              onChange={(e) => setJobDescription(e.target.value)}
+            />
           </label>
 
           <label className="seeker-ss3-textarea">
             <p>Your responsibilities:</p>
-            <textarea name="thesisDescription" />
+            <textarea
+              name="responsibilities"
+              value={responsibilities}
+              onChange={(e) => setResponsibilities(e.target.value)}
+            />
           </label>
 
           <label className="seeker-ss3-textarea">
             <p>Your achievements:</p>
-            <textarea name="thesisDescription" />
+            <textarea
+              name="achievements"
+              value={achievements}
+              onChange={(e) => setAchievements(e.target.value)}
+            />
           </label>
         </div>
 
@@ -82,7 +163,7 @@ export default function SignupSection5({ sectionRefs, handleNextSection }) {
       </div>
       <div className="seeker-ss-buttons">
         <p></p>
-        <button type="submit" id="section-add-button">
+        <button type="submit" id="section-add-button" onClick={handleSubmit}>
           <FaPlus />
           <p>Add Experience</p>
         </button>
