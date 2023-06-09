f50efe25e1f0b5b8a6a89c5f07b3aeebbcbc083b
Enock <enock.mokua@student.moringaschool.com>
2023-06-09 09:49:15 +0300

    make the navigational functional

--- src/Pages/recruiter/jobs/new/jobnew.jsx
+++ src/Pages/recruiter/jobs/new/jobnew.jsx
@@ -1,47 +1,96 @@
+import { useState } from "react";
 import RecruiterHeader from "../../../../Components/recruiter/header/header";
 import RecruiterAside from "../../../../Components/recruiter/navigation/navigation";
-// import "./newjob.css";
 import "./jobnew.css";
 
 function JobSummary() {
-  <form id="recruiter-job-summary" className="recruiter-nj-form"></form>;
+  return (
+    <form id="recruiter-job-summary" className="recruiter-nj-form">
+      Job Summary
+    </form>
+  );
 }
 
 function JobRequirements() {
-  <form id="recruiter-job-reqs" className="recruiter-nj-form"></form>;
+  return (
+    <form id="recruiter-job-reqs" className="recruiter-nj-form">
+      Job Requirements
+    </form>
+  );
 }
 
 function JobDetails() {
-  <form id="recruiter-job-dets" className="recruiter-nj-form"></form>;
+  return (
+    <form id="recruiter-job-dets" className="recruiter-nj-form">
+      Job Details
+    </form>
+  );
 }
 
 function JobSettings() {
-  <form id="recruiter-job-sets" className="recruiter-nj-form"></form>;
+  return (
+    <form id="recruiter-job-sets" className="recruiter-nj-form">
+      Job Settings
+    </form>
+  );
 }
 
 function JobPromote() {
-  <form id="recruiter-job-promote" className="recruiter-nj-form"></form>;
+  return (
+    <form id="recruiter-job-promote" className="recruiter-nj-form">
+      Job Promote
+    </form>
+  );
 }
 
 function JobCandidates() {
-  <form id="recruiter-job-cands" className="recruiter-nj-form"></form>;
+  return (
+    <form id="recruiter-job-cands" className="recruiter-nj-form">
+      Job Candidates
+    </form>
+  );
 }
 
 export default function RecruiterNewJob() {
+  const [currentStep, setCurrentStep] = useState(1);
+
+  const handleStepChange = (step) => {
+    setCurrentStep(step);
+  };
+
+  const renderStepComponent = () => {
+    switch (currentStep) {
+      case 1:
+        return <JobSummary />;
+      case 2:
+        return <JobRequirements />;
+      case 3:
+        return <JobDetails />;
+      case 4:
+        return <JobSettings />;
+      case 5:
+        return <JobPromote />;
+      case 6:
+        return <JobCandidates />;
+      default:
+        return null;
+    }
+  };
+
   return (
     <main className="recruiter">
       <RecruiterAside />
       <section className="recruiter-container">
         <RecruiterHeader />
         <section id="recruiter-new-jobs" className="recruiter-cont">
-          <div className="recruiter-new-job-cont"></div>
+          <div className="recruiter-new-job-cont">{renderStepComponent()}</div>
           <aside>
-            <button>1. Summary</button>
-            <button>2. Requirements</button>
-            <button>3. Details</button>
-            <button>4. Settings</button>
-            <button>5. Promote</button>
-            <button>6. Candidates</button>
+            <button onClick={() => handleStepChange(1)}>1. Summary</button>
+            <button onClick={() => handleStepChange(2)}>2. Requirements</button>
+            <button onClick={() => handleStepChange(3)}>3. Details</button>
+            <button onClick={() => handleStepChange(4)}>4. Settings</button>
+            <button onClick={() => handleStepChange(5)}>5. Promote</button>
+            <button onClick={() => handleStepChange(6)}>6. Candidates</button>
           </aside>
         </section>
       </section>
